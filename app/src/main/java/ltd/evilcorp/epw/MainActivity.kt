4b38481959994d79747b6a72f7ddab9fed926de1
Robin Linden <dev@robinlinden.eu>
2021-04-28 20:40:13 +0200

    Fix light/dark theme setting not being respected

--- app/src/main/java/ltd/evilcorp/epw/MainActivity.kt
+++ app/src/main/java/ltd/evilcorp/epw/MainActivity.kt
@@ -29,8 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EpwTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface {
                     AccountList(EXAMPLE_ACCOUNTS)
                 }
             }
